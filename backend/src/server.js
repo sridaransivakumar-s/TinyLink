0b67f692fe80683628b282299fffa3a5ef7eb28f
sridaransivakumar-s <sridaransivakumar22@gmail.com>
2025-11-20 18:03:17 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,10 @@ import webRouter from "./routes/web.js";
 import os from "os";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
